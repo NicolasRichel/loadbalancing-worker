26ac0505a425717c96f65779c4a2e8c1951bb7f1
NicolasRichel <nicolasrichel.github@gmail.com>
2019-03-21 10:34:14 +0100

    Rearrange tests folders

---
+++ tests/integration/worker.js
@@ -0,0 +1,75 @@
+/**
+ * Load Balancing Worker
+ * =====================
+ */
+
+// Initial state of the worker (Default values)
+let state = {
+  servers: [],
+  targetServer: '',
+  loadBalancingScript: '',
+  loadBalancingLoopID: null,
+  interval: 5000,
+  isActive: false
+};
+
+// Worker Actions
+const actions = {
+  initializeWorker(config) {
+    try {
+      actions.stopLoadBalancingLoop();
+      Object.assign(state, config);
+      self.importScript( state.loadBalancingScript );
+      return true;
+    } catch (err) {
+      return false;
+    }
+  },
+  startLoadBalancingLoop() {
+    if (!state.isActive) {
+      state.loadBalancingLoopID = setInterval(
+        () => loadBalancing( state.servers ).then(server => state.targetServer = server),
+        state.interval
+      );
+      state.isActive = true;
+      return true;
+    }
+    return false;
+  },
+  getServer() {
+    if (state.isActive) return state.targetServer;
+    return null;
+  },
+  isActive() {
+    return state.isActive;
+  },
+  stopLoadBalancingLoop() {
+    clearInterval( state.loadBalancingLoopID );
+    state.isActive = false;
+    return true;
+  },
+  destroyWorker() {
+    actions.stopLoadBalancingLoop();
+    self.close();
+  }
+};
+
+
+self.onmessage = (e) => {
+  let response = null;
+  switch (e.data.msg) {
+    case 'init':
+      response = actions.initializeWorker(e.data.config); break;
+    case 'start':
+      response = actions.startLoadBalancingLoop(); break;
+    case 'get-server':
+      response = actions.getServer(); break;
+    case 'is-active?':
+      response = actions.isActive(); break;
+    case 'stop':
+      response = actions.stopLoadBalancingLoop(); break;
+    case 'destroy':
+      actions.destroyWorker();
+  }
+  self.postMessage( response );
+};
--- tests/specification/init-test.js
+++ tests/specification/init-test.js
@@ -2,9 +2,9 @@ const NAME = 'Initialize';
 const DESCRIPTION = 'should initialize worker state via configuration';
 
 export function test() {
-
-  const worker = new Worker('/worker.js');
+  
   let result = false;
+  const worker = new Worker('/worker.js');
 
   worker.onmessage = (e) => result = e.data;
   worker.onerror = () => result = false;
@@ -16,6 +16,8 @@ export function test() {
       loadBalancingScript: '/tests/loadbalancing-test.js'
     }
   });
+
+  // TODO assert that worker state has been properly initialized
 
   return {
     testName: NAME,
